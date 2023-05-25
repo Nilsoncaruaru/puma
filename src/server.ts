3e4adcc12c83b25bddc5640bb04978d0c24a02ec
Nilsoncaruaru <nilsoncaruaru123@gmail.com>
2023-05-25 14:35:44 -0300

    crud functions on

--- src/server.ts
+++ src/server.ts
@@ -13,6 +13,47 @@ const server = fastify();
 server.register(cors, {
     //opções
 })
+server.post('/signup', async(request : any, reply: any) => {
+    const puma = await prisma.puma.create({
+        data: {
+            nome: request.body.nome, 
+            caracteristica: request.body.caracteristica,
+            vacina: request.body.vacina,
+            idade: request.body.idade
+        }
+      })
+    
+    return reply.status(201).send(puma);
+});
+
+server.get('/search', async(request, reply) => {
+    const puma = await prisma.puma.findMany()
+
+    return puma;
+})
+
+server.put('/update', async(request: any, reply: any) =>{
+    const IdAnimal = await prisma.puma.update({
+        where: {
+            IdAnimal: request.body.IdAnimal
+        },
+        data: {
+            nome: request.body.nome,
+            caracteristica: request.body.caracteristica,
+            vacina: request.body.vacina,
+            idade: request.body.idade
+        }
+    })
+})
+
+server.delete('/delete/:IdAnimal', async(request: any, reply: any) =>{
+    const puma = await prisma.puma.delete({
+        where: {
+            IdAnimal: request.body.IdAnimal
+        }
+    })
+    return "animal deletado";
+}) 
 
 server.listen({ port }, (error, address) => {
     if (error) {
